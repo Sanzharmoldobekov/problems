d9de7315a7283a429410c598826b8eaf430f659e
Sanzharmoldobekov <103410435+Sanzharmoldobekov@users.noreply.github.com>
2022-04-14 14:25:59 +0600

    Add files via upload

---
+++ iskluchenie.py
@@ -0,0 +1,49 @@
+#indexerror
+#x=('1','2','3','4')
+#print(x[4])
+
+#nameerror
+#x=1
+#print(a)
+
+#typeerror
+#x=6
+#a='3'
+#print(x-a)
+
+#problem2
+#at = 10
+#x = 15
+#wo = 20
+#for e in  range(at):
+#	print(wo / at)
+#if at > 5:
+#	print(at > 5)
+
+#prooblem3
+#a = 0
+#b = 1
+#numbers = 4
+#while b > a:
+#	numbers += b
+#	b += 1
+#	print(numbers)
+#	break
+
+#problem4
+
+lst = []
+
+for i in range(10):
+
+	lst.append(i)
+
+
+	list(reversed(lst))
+
+	sls_obj = slice('0','8')
+
+	print(sls_obj)
+
+
+
