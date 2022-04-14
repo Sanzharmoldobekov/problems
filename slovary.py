c91c187c38aa8db84ec850ce4a08a305a8bf0bfa
Sanzharmoldobekov <103410435+Sanzharmoldobekov@users.noreply.github.com>
2022-04-14 14:51:09 +0600

    Add files via upload

---
+++ slovary.py
@@ -0,0 +1,29 @@
+#1 problem
+#menu = {'lagman': '120', 'plov': '120', 'borsh': '100'}
+#menu.update({'drinks':'Coca-cola,Sprite,Fanta-90'})
+#print(menu)
+
+#2 problem
+#x={}
+#a=input('Введите имя')
+#b=input('Введите имя')
+#c=input('Введите имя')
+#y=int(input('Введите пароль'))
+#y1=int(input('Введите пароль'))
+#y2=int(input('Введите пароль'))
+#x={a: y,b:y1,c:y2}
+#print(x)
+
+#3 problem
+#menu = {'besh_barmak':'130','lagman': '120', 'plov': '120', 'borsh': '100'}
+#menu.update({'besh_barmak':'135'})
+#del menu['borsh']
+#print(menu)
+
+#4 problem
+south_american=set()
+south_american = ['Argentina', 'Bolivia', 'Brazil', 'Chile', 'Colombia', 'Ecuador', 'Guyana', 'Paraguay', 'Peru', 'Suriname', 'Suriname', 'Uruguay', 'Venezuela']
+gf = {}
+gh = {i: south_american[i] for i in range(len(south_american))}
+print(south_american)
+
