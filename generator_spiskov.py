e9e13cf2c76560c2ddfa809d4e0341e40cd40ad6
Sanzharmoldobekov <103410435+Sanzharmoldobekov@users.noreply.github.com>
2022-05-03 13:01:22 +0600

    Add files via upload

---
+++ generator_spiskov.py
@@ -0,0 +1,15 @@
+'''
+import random
+k=random.randint(1,255)
+if k==15 and k==24 and k==81 and 132==k and k==196 and 214==k:
+    print('запрещенное число')
+print(k)
+
+#Сгенерируйте список с матрицой внутри с размерностью 6х6
+i,j = 6,6
+for _ in range(0, i*j, j):
+    print('5'* j)
+'''
+names = ['Kairat', 'Erlan', 'Dastan', 'Artur', 'Bahtoolot', 'Azat', 'Bakyt']
+names.sort()
+print(names)
